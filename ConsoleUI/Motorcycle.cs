26a9e3fe8c7f3b03a4679b0bc978969894ef4897
Stephen Michaels <stephen.michaels3333@gmail.com>
2023-03-01 13:55:45 -0500

    inital commit

---
+++ ConsoleUI/Motorcycle.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleUI
+{
+    public class Motorcycle : Vehicle
+    {
+        public Motorcycle() 
+        {
+            
+        }
+        public bool HasSideCar { get; set; } = false;
+        public override void DriveAbstract()
+        {
+            Console.WriteLine("This motorcycle is in drive");
+        }
+        public override void DriveVirtual()
+        {
+            Console.WriteLine($"This { GetType().BaseType.Name} is a {GetType().BaseType.Name} that is virtually in drive");
+        }
+    }
+}
--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -17,32 +17,48 @@ namespace ConsoleUI
             #region Vehicles
 
             /*
-             * Create an abstract class called Vehicle
-             * The vehicle class shall have three string properties Year, Make, and Model
-             * Set the defaults to something generic in the Vehicle class
-             * Vehicle shall have an abstract method called DriveAbstract with no implementation
-             * Vehicle shall have a virtual method called DriveVirtual with a base implementation.
+             * Done - Create an abstract class called Vehicle
+             * Done - The vehicle class shall have three string properties Year, Make, and Model
+             * Done - Set the defaults to something generic in the Vehicle class
+             * Done - Vehicle shall have an abstract method called DriveAbstract with no implementation
+             * Done - Vehicle shall have a virtual method called DriveVirtual with a base implementation.
              */
 
             /* 
-             * Now create 2 non-abstract classes: Car and Motorcycle, that inherit from Vehicle
-             * Add a distict property in the 2 derived classes such as HasTrunk for Car and HasSideCart for Motorcycle
-             * Provide the implementations for the abstract methods
-             * Only in the Motorcycle class will you override the virtual drive method
+             * DONE - Now create 2 non-abstract classes: Car and Motorcycle, that inherit from Vehicle
+             * DONE - Add a distict property in the 2 derived classes such as HasTrunk for Car and HasSideCart for Motorcycle
+             * DONE - Provide the implementations for the abstract methods
+             * DONE - Only in the Motorcycle class will you override the virtual drive method
             */
 
-            // Create a list of Vehicle called vehicles
+            // DONE - Create a list of Vehicle called vehicles
+            var vehicles = new List<Vehicle>();
 
             /*
              * Create 4 instances: 1 Car, 1 Motorcycle, and then 2 instances of type Vehicle (use explicit typing) but use constuctors from derived classes
              * 
              * Set the properties with object initializer syntax
              */
+            Car focus = new Car() { HasTrunk= true, Make = "Ford", Model = "Focus", Year = 2013 };
+            Motorcycle harley = new Motorcycle() { HasSideCar = true, Make = "Harley Davidson", Model = "Fatboy", Year = 2018 };
+
+            Vehicle sedan = new Car() { HasTrunk = true, Make = "Kia", Model = "Forte", Year = 2019 };
+            Vehicle sport = new Car() { HasTrunk = true, Make = "Chevy", Model = "Corvette", Year = 2018 };
 
             /*
              * Add the 4 vehicles to the list
              * Using a foreach loop iterate over each of the properties
              */
+            vehicles.Add(focus);
+            vehicles.Add(harley);
+            vehicles.Add(sedan);
+            vehicles.Add(sport);
+
+            foreach(var vehicle in vehicles)
+            {
+                Console.WriteLine($"Make {vehicle.Make} Model {vehicle.Model} Year {vehicle.Year}");
+                vehicle.DriveAbstract();
+            }
 
             // Call each of the drive methods for one car and one motorcycle
 
